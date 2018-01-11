a32a28afb9336231ad52fc45be8649671ad50182
Nonii Xu <nonii@qq.com>
2018-01-12 02:07:35 +0800

    Monkey sort

--- MonkeySort/Program.cs
+++ MonkeySort/Program.cs
@@ -7,6 +7,18 @@ using System.Threading.Tasks;
 namespace MonkeySort {
     class Program {
         static void Main ( string [] args ) {
+            var rnd = new Random ( );
+            var data = ( from i in new int [10] select rnd.Next ( 20 ) ).ToArray ( );
+
+            data.ToList ( ).ForEach ( di => Console.Write ( di + ", " ) );
+            Console.WriteLine ( );
+
+            var sort = new MonkeySort<int> ( ( x, y ) => x <= y );
+            sort.Sort ( ref data );
+
+            data.ToList ( ).ForEach ( di => Console.Write ( di + ", " ) );
+
+            Console.ReadKey ( );
         }
     }
 }
