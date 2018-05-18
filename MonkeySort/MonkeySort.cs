a82f81e8aca82d9a9ce1a8aec789b9b549a4be5e
Nonii Xu <nonii@qq.com>
2018-05-18 17:44:50 +0800

    code formatting

--- MonkeySort/MonkeySort.cs
+++ MonkeySort/MonkeySort.cs
@@ -1,52 +1,60 @@
 ﻿using System;
 
-namespace MonkeySort {
-    public class MonkeySort<T> {
-        private Func<T, T, bool> judge;
+namespace MonkeySort
+{
+    public class MonkeySort<T>
+    {
+        private readonly Func<T, T, bool> judge;
 
-        public MonkeySort ( Func<T, T, bool> judge_func ) => judge = judge_func;
+        public MonkeySort(Func<T, T, bool> judge_func)
+        {
+            judge = judge_func;
+        }
 
-        public void Sort ( ref T [] data ) {
-            if ( data == null ) {
-                throw new ArgumentNullException ( );
+        public void Sort(ref T[] data)
+        {
+            if (data == null) {
+                throw new ArgumentNullException();
             }
 
-            var rnd = new Random ( );
-            while ( IsDataSorted ( data ) == false ) {
-                _SwitchDataItem ( ref data, rnd.Next ( 0, data.Length ), rnd.Next ( 0, data.Length ) );
+            var rnd = new Random();
+            while (IsDataSorted(data) == false) {
+                _SwitchDataItem(ref data, rnd.Next(0, data.Length), rnd.Next(0, data.Length));
             }
         }
 
-        public bool IsDataSorted ( T [] data ) {
-            if ( data == null ) {
-                throw new ArgumentNullException ( );
+        public bool IsDataSorted(T[] data)
+        {
+            if (data == null) {
+                throw new ArgumentNullException();
             }
-            if ( data.Length <= 1 ) {
+            if (data.Length <= 1) {
                 return true;
             }
 
-            for ( var i = 1; i < data.Length; i++ ) {
-                if ( judge ( data [i - 1], data [i] ) == false ) {
+            for (var i = 1; i < data.Length; i++) {
+                if (judge(data[i - 1], data[i]) == false) {
                     return false;
                 }
             }
             return true;
         }
 
-        private void _SwitchDataItem ( ref T [] data, int i, int j ) {
-            if ( data == null ) {
-                throw new ArgumentNullException ( );
+        private void _SwitchDataItem(ref T[] data, int i, int j)
+        {
+            if (data == null) {
+                throw new ArgumentNullException();
             }
-            if ( i < 0 || j < 0 || i >= data.Length || j >= data.Length ) {
-                throw new ArgumentException ( );
+            if (i < 0 || j < 0 || i >= data.Length || j >= data.Length) {
+                throw new ArgumentException();
             }
-            if ( i == j ) {
+            if (i == j) {
                 return;
             }
 
-            var tmp = data [i];
-            data [i] = data [j];
-            data [j] = tmp;
+            var tmp = data[i];
+            data[i] = data[j];
+            data[j] = tmp;
         }
     }
 }
--- MonkeySort/Program.cs
+++ MonkeySort/Program.cs
@@ -4,21 +4,24 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MonkeySort {
-    class Program {
-        static void Main ( string [] args ) {
-            var rnd = new Random ( );
-            var data = ( from i in new int [10] select rnd.Next ( 20 ) ).ToArray ( );
+namespace MonkeySort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var rnd = new Random();
+            var data = (from i in new int[10] select rnd.Next(20)).ToArray();
 
-            data.ToList ( ).ForEach ( di => Console.Write ( di + ", " ) );
-            Console.WriteLine ( );
+            data.ToList().ForEach(di => Console.Write(di + ", "));
+            Console.WriteLine();
 
-            var sort = new MonkeySort<int> ( ( x, y ) => x <= y );
-            sort.Sort ( ref data );
+            var sort = new MonkeySort<int>((x, y) => x <= y);
+            sort.Sort(ref data);
 
-            data.ToList ( ).ForEach ( di => Console.Write ( di + ", " ) );
+            data.ToList().ForEach(di => Console.Write(di + ", "));
 
-            Console.ReadKey ( );
+            Console.ReadKey();
         }
     }
 }
